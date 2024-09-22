eda2296cbab99c8b59fdb7873cfdf378d3ff3172
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-09-22 15:45:22 +0530

    changes of http implemented

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
 require("dotenv").config();
+
+const key = fs.readFileSync(__dirname + "/selfsigned.key");
+const cert = fs.readFileSync(__dirname + "/selfsigned.crt");
+const options = {
+  key: key,
+  cert: cert,
+};
 
 //app
 const app = express();
@@ -30,4 +39,9 @@ readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 //port
 const port = process.env.PORT || 8000;
+const portSSL = process.env.PORT_SSL || 8443;
 app.listen(port, () => console.log(`Server is running on ${port}`));
+const server = https.createServer(options, app);
+server.listen(portSSL, () =>
+  console.log(`Https server is running on ${portSSL}`)
+);
